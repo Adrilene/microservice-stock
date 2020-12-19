417e404bff76f5dda1bad651b9b5fdc8d03223fc
adrilene <adrilenefonseca@gmail.com>
2020-12-19 11:16:02 -0300

    done

---
+++ stock/controllers/stock_controller.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+from stock.services.stock_service import get_quantity, insert_product
+from flask_restful import Resource
+from stock import app, api
+import json
+
+
+class StockQuantityController(Resource):
+    def get(self, _id):
+        product = get_quantity(_id)
+        product = json.loads(product.to_json())
+        return{"quantity": product["quantity"]}, 200
+
+
+class StockController(Resource):
+    def post(self):
+        data_json = {
+            "product_id": request.form["product_id"],
+            "quantity": request.form["quantity"],
+        }
+        insert_product(data_json)
+        return "OK", 200
+
+
+api.add_resource(StockQuantityController, "/stock/<_id>")
+api.add_resource(StockController, "/stock")
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from stock import app
+
+
+if __name__ == "__main__":
+    app.run(port=5002, debug=True)
