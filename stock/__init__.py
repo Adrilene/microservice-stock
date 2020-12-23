664e15a5ab27b9180e2f8b35baf0d4a28d6fc7fa
Denis Menezes de Sousa <denismzsousa@gmail.com>
2020-12-23 00:07:19 -0300

    in progress

--- stock/__init__.py
+++ stock/__init__.py
@@ -6,8 +6,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {
-    "db": "devices",
-    "host": "mongodb://localhost/stock",
+    "db": "stock",
+    "host": "mongodb+srv://adrilene:arq2201@cluster0.a3msk.mongodb.net/test?authSource=admin&replicaSet=atlas-2nqljv-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
 }
 db = MongoEngine(app)
 api = Api(app)
